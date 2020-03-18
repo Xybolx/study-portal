b448a377b9223a2d5358b32b3d7d7ca81c2e685f
Mat Hayward <xybolx@Mats-MacBook-Pro.local>
2020-03-18 10:20:16 -0700

    port to PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require("express-session");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express(); 
-const port = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -30,6 +30,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/power", { useNe
 app.use(routes);
 
 // Start the API server
-app.listen(port, function() {
+app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
